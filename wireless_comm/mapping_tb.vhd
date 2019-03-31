77fb1b6bf5ac7d7f9d0a978186c0a50050290d66
Astro <m.rahm7n@gmail.com>
2019-03-31 09:38:43 +0200

    Create mapping_tb.vhd

---
+++ wireless_comm/mapping_tb.vhd
@@ -0,0 +1,11 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+ENTITY mapping_tb is
+END mapping_tb;
+
+ARCHITECTURE Behavioral of mapping_tb is
+begin
+
+end ARCHITECTURE
