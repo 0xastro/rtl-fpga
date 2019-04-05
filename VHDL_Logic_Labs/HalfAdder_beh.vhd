786617d455b95efc2af942b515779a5c928a9208
Astro <m.rahm7n@gmail.com>
2019-04-05 08:15:56 +0200

    Update and rename HalfAdder1.vhd to HalfAdder_beh.vhd

--- VHDL_Logic_Labs/HalfAdder_beh.vhd
+++ VHDL_Logic_Labs/HalfAdder_beh.vhd
@@ -1,27 +1,28 @@
 ```VHDL
 -------------------------------
 -- HalfAdder Implementation
+-- Testbench is Included under the name HalfAdder_beh_tb
 -------------------------------
 entity HalfAdder is
 	port (
-		a	: in std_logic;
-		b	: in STD_LOGIC;	
-		co	: out std_logic;
-		s	: out std_logic
+		i_a	: in	std_logic;
+		i_b	: in	std_logic;	
+		o_sum	: out	std_logic;
+		o_carry	: out	std_logic
 		);
 end HalfAdder;
 
 architecture Beh of HalfAdder is -- architecture name
 	begin
-		comb_p: process(a,b)
+		comb_p: process(i_a,i_b)
 			begin
-				if (a and b) = '1' then
-					co <= '1';
+				if (i_a and i_b) = '1' then
+					o_carry <= '1';
 				else
-					co <= '0';
+					o_carry <= '0';
 				end if;
 			end process
 		s <= a xor b;
-	end Beh;
+end Beh;
   
 ```
